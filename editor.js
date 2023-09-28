f6a86186ef4f65b25cc71be8ae508c1da78c553f
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-09-28 10:28:52 -0700

    update editor

--- editor.js
+++ editor.js
@@ -1,7 +1,3 @@
-javascript:(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.documentElement.appendChild(script); script.onload = function () { eruda.init() } })();
-
-
-
 var history = ['']
 var histI = 0
 var histIsM = true
