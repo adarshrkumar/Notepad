575907d557d6907a9e07c9b42503480d7e381028
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-02 11:02:08 -0700

    update editor

--- actions.js
+++ actions.js
@@ -5,3 +5,28 @@ var action = urlParams.get('action')
 if (action === 'upload') {
     document.querySelector(`input[type='file']#readfile`).click()
 }
+else if (action === 'open') {
+    var fName = urlParams.get('file')
+    var file = localStorage.getItem(`FILEDATA://${fName}`)
+    file = JSON.parse(file)
+
+    let textarea = document.querySelector('main textarea')
+    // json = {
+    //     title: file.title, 
+    //     content: file.value, 
+    //     author: file.author, 
+    //     dateModofied: file.dateModofied, 
+    // }
+
+    fName = file.title
+    if (fName.includes('.')) {
+        nFName = fName.split('.')
+        nFName = nFName[nFName.length-1]
+        if (fName === 'txt') {
+            fName = fName.substring(fName.split('').length-4)
+        }
+    }
+
+    document.getElementById('title').value = fName
+    textarea.value = file.value
+}
