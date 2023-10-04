8568a7277e333a1f89fd4f794b7dcbacc581ffd5
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-04 11:56:59 -0700

    update editor

--- editor.js
+++ editor.js
@@ -237,24 +237,28 @@ function editFile(fName) {
   location.href = `${location.pathname}?action=open&file=${fName}`
 }
 
-if (!!document.getElementById('editBtn')) document.getElementById('editBtn').addEventListener('click', editFile)
+function addEventListeners() {
+  if (!!document.getElementById('editBtn')) document.getElementById('editBtn').addEventListener('click', editFile)
 
-if (!!document.getElementById('download')) document.getElementById('download').addEventListener('click', download)
-if (!!document.getElementById('reset')) document.getElementById('reset').onclick = reset
-
-if (!!document.getElementById('undo')) document.getElementById('undo').addEventListener('click', undo)
-if (!!document.getElementById('redo')) document.getElementById('redo').addEventListener('click', redo)
-
-if (!!document.getElementById('replaceThis')) document.getElementById('replaceThis').addEventListener('click', function(e) { replace('single', e) })
-if (!!document.getElementById('replaceAll')) document.getElementById('replaceAll').addEventListener('click', function(e) { replace('all', e) })
-
-if (!!document.getElementById('shareLink')) document.getElementById('shareLink').addEventListener('click', getShareLink)
-
-if (!!document.getElementById('takeFile')) {
-  document.getElementById('takeFile').onclick = function(e) {
-    document.querySelector(`input[type='file']#readfile`).click()
+  if (!!document.getElementById('download')) document.getElementById('download').addEventListener('click', download)
+  if (!!document.getElementById('reset')) document.getElementById('reset').onclick = reset
+  
+  if (!!document.getElementById('undo')) document.getElementById('undo').addEventListener('click', undo)
+  if (!!document.getElementById('redo')) document.getElementById('redo').addEventListener('click', redo)
+  
+  if (!!document.getElementById('replaceThis')) document.getElementById('replaceThis').addEventListener('click', function(e) { replace('single', e) })
+  if (!!document.getElementById('replaceAll')) document.getElementById('replaceAll').addEventListener('click', function(e) { replace('all', e) })
+  
+  if (!!document.getElementById('shareLink')) document.getElementById('shareLink').addEventListener('click', getShareLink)
+  
+  if (!!document.getElementById('takeFile')) {
+    document.getElementById('takeFile').onclick = function(e) {
+      document.querySelector(`input[type='file']#readfile`).click()
+    }
   }
+  
+  
+  if (!!document.getElementById('enableHTML')) document.getElementById('enableHTML').addEventListener('click', checkHTML)
 }
 
-
-if (!!document.getElementById('enableHTML')) document.getElementById('enableHTML').addEventListener('click', checkHTML)
+window.addEventListener('DOMContentLoaded', addEventListeners)
