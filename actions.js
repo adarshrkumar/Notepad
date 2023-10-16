6aafd55d3bcc2e21dc3769f3d4e0a9dac9f769c2
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-16 12:40:58 -0700

    update actions

--- actions.js
+++ actions.js
@@ -2,87 +2,83 @@ var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
 var action = urlParams.get('action')
 
-switch(action) {
-    case 'upload':
-        addEventListener('DOMContentLoaded', clickUploadElement)
-        clickUploadElement()
-        break;
-        return;
-    case 'openfile':
-        var fName = urlParams.get('file')
-        var file = localStorage.getItem(`FILEDATA://${fName}`)
-        file = JSON.parse(file)
-    
-        let textarea = document.querySelector('main textarea')
-    
-        var fName = String(file.title)
-        if (fName.includes('.')) {
-            nFName = fName.split('.')
-            nFName = nFName[nFName.length-1]
-            if (fName === 'txt') {
-                fName = fName.substring(fName.split('').length-4)
-            }
+if (action === 'upload') {
+    addEventListener('DOMContentLoaded', clickUploadElement)
+    clickUploadElement()
+}
+else if (action === 'openfile') {
+    var fName = urlParams.get('file')
+    var file = localStorage.getItem(`FILEDATA://${fName}`)
+    file = JSON.parse(file)
+
+    let textarea = document.querySelector('main textarea')
+
+    var fName = String(file.title)
+    if (fName.includes('.')) {
+        nFName = fName.split('.')
+        nFName = nFName[nFName.length-1]
+        if (fName === 'txt') {
+            fName = fName.substring(fName.split('').length-4)
         }
-    
-        var fContent = file.content
-        if (!!fContent === false) {
-            fContent = 'File Content Not Found or Corrupted'
+    }
+
+    var fContent = file.content
+    if (!!fContent === false) {
+        fContent = 'File Content Not Found or Corrupted'
+    }
+
+    document.getElementById('title').value = fName
+    textarea.value = file.content
+}
+else if (action === 'filelink') {
+    let fName = urlParams.get('file')
+    let fContent = urlParams.get('content')
+    fContent = atob(fContent)
+
+    let disabledEles = []
+
+    let titleEle = document.getElementById('title')
+    titleEle.value = fName
+    disabledEles.push(titleEle)  
+
+    let textAreaEle = document.querySelector('main textarea')
+    textAreaEle.value = fContent
+    disabledEles.push(textAreaEle)
+
+    disabledEles.forEach(function(ele, i) {
+        ele.setAttribute('disabled', '')  
+        ele.style.cursor = 'pointer'
+    })
+
+    let removeEles = [
+        'enableHTML', 
+        'undo', 
+        'redo', 
+        'replacethis', 
+        'withthis', 
+        'replaceThis', 
+        'replaceAll', 
+    ]
+
+    removeEles.forEach(function(e, i) {
+        let element = document.querySelector(e)
+        if (!!element === false) {
+            element = document.getElementById(e)
         }
-    
-        document.getElementById('title').value = fName
-        textarea.value = file.content
-        break;
-        return;
-    case 'filelink':
-        let fName = urlParams.get('file')
-        let fContent = urlParams.get('content')
-        fContent = atob(fContent)
-    
-        let disabledEles = []
-    
-        let titleEle = document.getElementById('title')
-        titleEle.value = fName
-        disabledEles.push(titleEle)  
-    
-        let textAreaEle = document.querySelector('main textarea')
-        textAreaEle.value = fContent
-        disabledEles.push(textAreaEle)
-    
-        disabledEles.forEach(function(ele, i) {
-            ele.setAttribute('disabled', '')  
-            ele.style.cursor = 'pointer'
-        })
-    
-        let removeEles = [
-            'enableHTML', 
-            'undo', 
-            'redo', 
-            'replacethis', 
-            'withthis', 
-            'replaceThis', 
-            'replaceAll', 
-        ]
-    
-        removeEles.forEach(function(e, i) {
-            let element = document.querySelector(e)
-            if (!!element === false) {
-                element = document.getElementById(e)
-            }
-            if (!!element) {
-                element.parentNode.setAttribute('hidden', '')
-            }
-        })
-    
-        document.getElementById('takeFile').onclick = function(e) {}
-        document.getElementById('takeFile').parentNode.href = `${location.pathname}?action=upload`
-    
-        document.getElementById('reset').onclick = function() {}
-        document.getElementById('reset').parentNode.href = `${location.pathname}?action=new`
-    
-        let editBtn = document.getElementById('editFile')
-        editBtn.parentNode.removeAttribute('hidden')
-        break;
-        return;
-    default:
-      
+        if (!!element) {
+            element.parentNode.setAttribute('hidden', '')
+        }
+    })
+
+    document.getElementById('takeFile').onclick = function(e) {}
+    document.getElementById('takeFile').parentNode.href = `${location.pathname}?action=upload`
+
+    document.getElementById('reset').onclick = function() {}
+    document.getElementById('reset').parentNode.href = `${location.pathname}?action=new`
+
+    let editBtn = document.getElementById('editFile')
+    editBtn.parentNode.removeAttribute('hidden')
+}
+else {
+
 }
