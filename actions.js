0e60d4be554284f96a99935cc2f2c6fbb4e73c07
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-18 10:59:52 -0700

    update actions

--- actions.js
+++ actions.js
@@ -2,7 +2,7 @@ var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
 var action = urlParams.get('action')
 
-if (action === 'upload') {
+if (action === 'upload' || action === 'new') {
     
 }
 else if (action === 'openfile') {
