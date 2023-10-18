9c407b1fda18344db36853e941e132a830291aa4
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-18 10:21:40 -0700

    update settings

---
+++ settings.js
@@ -0,0 +1,29 @@
+var items = [
+    {
+        id: 'username', 
+        event: 'onchange', 
+
+    }, 
+    {
+        id: '', 
+    }, 
+]
+
+items.forEach(function(item, i) {
+    var element = document.getElementById(item.id)
+    if (!!element === false) element = document.querySelector(item.id)
+    if (!!element) {
+        if (item.event.startsWith('on')) element[item.event] = function() {
+            saveToLocalStorage(item, selector)
+        }
+        else {
+            element.addEventListener(item.event, function() {
+                saveToLocalStorage(item, selector)
+            })
+        }
+    }
+})
+
+function saveToLocalStorage(item, selector) {
+    localStorage.setItem(item, selector.value)
+}
