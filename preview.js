463a936e5baa56beaa9e655bf0963294cb9d01e5
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-31 12:43:35 -0700

    update preview

--- preview.js
+++ preview.js
@@ -39,7 +39,7 @@ function addCode() {
           ELEMENTS[i] = ''
         }
       })
-      document[parent.document.getElementById('eleVal')].innerHTML = code + needsClose + bodyHTML
+      document[parent.document.getElementById('eleVal').value].innerHTML = code + needsClose + bodyHTML
       lastCode = code
     }
   }
