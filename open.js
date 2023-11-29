b090e73d4834b779f5033c2647ad7ec2ef1027f1
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2023-11-29 11:04:25 -0800

    update open

--- open.js
+++ open.js
@@ -60,7 +60,7 @@ filesObj.forEach(function(fileName, i) {
   deleteOption.classList.add('delete')
 
   deleteOption.textContent = 'delete'
-  deleteOption.href = `javascript:deleteFile('.${deleteOption.classList.join('.')}')` 
+  deleteOption.href = `javascript:deleteFile('.${deleteOption.className.split(' ').join('.')}')` 
 
   optionsElement.appendChild(deleteOption)
 
