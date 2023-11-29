17c1629fcfdeba1c84835e0a4dc2a3494604d7b2
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2023-11-29 10:42:50 -0800

    update open.js

--- open.js
+++ open.js
@@ -9,63 +9,63 @@ else {
 let filesEle = document.querySelector('#files > content')
 
 filesObj.forEach(function(fileName, i) {
-    let file = localStorage.getItem(`FILEDATA://${fileName}`)
-    file = JSON.parse(file)
-    let title = file.title
-    let content = file.content
-    let author = file.author
-    let dateModofied = file.dateModofied
+  let file = localStorage.getItem(`FILEDATA://${fileName}`)
+  file = JSON.parse(file)
+  let title = file.title
+  let content = file.content
+  let author = file.author
+  let dateModofied = file.dateModofied
 
-    let fileParent = document.createElement('div')
-    fileParent.classList.add('grid-parent')
+  let fileParent = document.createElement('div')
+  fileParent.classList.add('grid-parent')
 
-    let fileElement = document.createElement('a')
-    fileElement.classList.add('grid-item')
-    fileElement.href =  `editor?action=openfile&file=${title}`
-    fileParent.appendChild(fileElement)
+  let fileElement = document.createElement('a')
+  fileElement.classList.add('grid-item')
+  fileElement.href =  `editor?action=openfile&file=${title}`
+  fileParent.appendChild(fileElement)
 
-    let img = document.createElement('span')
-    img.classList.add('img')
-    img.classList.add('material-symbols-outlined')
-    img.textContent = 'description'
-    fileElement.appendChild(img)
+  let img = document.createElement('span')
+  img.classList.add('img')
+  img.classList.add('material-symbols-outlined')
+  img.textContent = 'description'
+  fileElement.appendChild(img)
 
-    let titleParent = document.createElement('div')
-    titleParent.classList.add('title-parent')
+  let titleParent = document.createElement('div')
+  titleParent.classList.add('title-parent')
 
-    let titleElement = document.createElement('h2')
-    titleElement.textContent = title
-    titleElement.classList.add('title')
-    titleParent.appendChild(titleElement)
+  let titleElement = document.createElement('h2')
+  titleElement.textContent = title
+  titleElement.classList.add('title')
+  titleParent.appendChild(titleElement)
 
-    fileElement.appendChild(titleParent)
-    
-    let authorElement = document.createElement('span')
-    authorElement.textContent = author
-    authorElement.classList.add('author')
-    fileElement.appendChild(authorElement)
+  fileElement.appendChild(titleParent)
+  
+  let authorElement = document.createElement('span')
+  authorElement.textContent = author
+  authorElement.classList.add('author')
+  fileElement.appendChild(authorElement)
 
-    let dateElement = document.createElement('span')
-    dateElement.textContent = dateModofied
-    dateElement.classList.add('date')
-    fileElement.appendChild(dateElement)
+  let dateElement = document.createElement('span')
+  dateElement.textContent = dateModofied
+  dateElement.classList.add('date')
+  fileElement.appendChild(dateElement)
 
-    let optionsElement = document.createElement('div')
-    optionsElement.classList.add('options')
+  let optionsElement = document.createElement('div')
+  optionsElement.classList.add('options')
 
-    let deleteOption = document.createElement('a')
+  let deleteOption = document.createElement('a')
 
-    deleteOption.classList.add('option')
-    deleteOption.classList.add('material-symbols-outlined')
+  deleteOption.classList.add('option')
+  deleteOption.classList.add('material-symbols-outlined')
 
-    deleteOption.textContent = 'delete'
-    deleteOption.href = `javascript:deleteFile('.${deleteFile.classList.join('.')}')` 
+  deleteOption.textContent = 'delete'
+  deleteOption.href = `javascript:deleteFile('.${deleteFile.classList.join('.')}')` 
 
-    optionsElement.appendChild(deleteOption)
+  optionsElement.appendChild(deleteOption)
 
-    fileParent.appendChild(optionsElement)
+  fileParent.appendChild(optionsElement)
 
-    filesEle.appendChild(fileParent)
+  filesEle.appendChild(fileParent)
 });
 
 function deleteFile(e) {
