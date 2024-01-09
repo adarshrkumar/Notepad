c2ea2700c3f72961b5aa84181eac62bdc19dd6fb
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-01-08 16:00:12 -0800

    opdate editor

--- sectionWidth.js
+++ sectionWidth.js
@@ -1,19 +1,14 @@
-let section = document.querySelector('section')
+let toolbar = document.querySelector('.toolbar')
 let sWidth = 8*2
-let eS = document.querySelectorAll('section > *')
+let eS = document.querySelectorAll('.toolbar > *')
 let hasDone = false
 eS.forEach(e => {
-  if (e.querySelector('*').id !== 'readfile') {
-    sWidth+=e.clientWidth
-    sWidth+=8
-    hasDone = true
-  }
+  sWidth+=e.clientWidth
+  sWidth+=8
+  hasDone = true
 })
-if (hasDone) {
-  sWidth-=8
-}
 
 let wWidth = window.innerWidth
 if (sWidth >= wWidth) {
-  section.style = 'overflow-x: scroll !important; margin-bottom: 0px;'
+  toolbar.style.paddingBottom = '0'
 }
