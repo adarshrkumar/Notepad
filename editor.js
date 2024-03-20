9956b21e8444efcf079409703f36cf2e9314fc76
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-03-20 11:41:03 -0700

    update editor.js

--- editor.js
+++ editor.js
@@ -124,6 +124,7 @@ function checkImage(file, name) {
   var isImage = false
   supptdImgExts.forEach(function(g) {
     g.forEach(function(e, i) {
+      console.log(`${ext} === ${e}`)
       if (ext === e) {
         textarea.value = `<style>
   img {
