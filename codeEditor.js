adc10659fe2b3ece74bd5e48eba8230cef149d31
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-02-28 09:52:35 -0800

    create and use code.html

---
+++ codeEditor.js
@@ -0,0 +1,21 @@
+require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' }});
+window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+
+let proxy = URL.createObjectURL(new Blob([`
+    self.MonacoEnvironment = {
+        baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
+    };
+    importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+`], { type: 'text/javascript' }));
+
+require(["vs/editor/editor.main"], function () {
+    let editor = monaco.editor.create(document.getElementById('container'), {
+        value: [
+            'function x() {',
+            '    console.log("Hello world!");',
+            '}'
+        ].join('\n'),
+        language: 'javascript',
+        theme: 'vs-dark'
+    });
+});
