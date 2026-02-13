ebf124726bc1d8d47e35f1675c910aeaee1295c0
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-02-12 16:20:22 -0800

    u

--- sectionWidth.js
+++ sectionWidth.js
@@ -3,12 +3,12 @@ let sWidth = 8*2
 let eS = document.querySelectorAll('.toolbar > *')
 let hasDone = false
 eS.forEach(e => {
-  sWidth+=e.clientWidth
-  sWidth+=8
-  hasDone = true
+    sWidth+=e.clientWidth
+    sWidth+=8
+    hasDone = true
 })
 
 let wWidth = window.innerWidth
 if (sWidth >= wWidth) {
-  toolbar.style.paddingBottom = '0'
+    toolbar.style.paddingBottom = '0'
 }
